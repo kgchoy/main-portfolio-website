8d9fa3cd5aefe8840a966e10258498c9504f715f
Kevin Choy <kevinc.2052@gmail.com>
2015-12-13 02:09:25 -0800

    added an extra row of work; fixed display issues with uneven description line counts

--- site/snippets/mainPageProjects.php
+++ site/snippets/mainPageProjects.php
@@ -3,53 +3,165 @@
                 <h2>Featured Work</h2>
 
                 <div class="grid">
-                    <div class="main-page-work-grid">
-                        <div class="col-1of3-work-main">
-                            
 
-                            <div class="col-1of3-work-figure">
-                                <div class="main-work-listing-figure">
-                                    <figure>
-                                        <a href="<?php echo page('work/bccampus') ?>"><img class="work-listing-img" src="content/home/bcc-website-screenshot-cropped.jpg" alt="BCcampus website screenshot"></a>
-                                    </figure>
+                    <div class="main-page-work-row">
+                        <!-- item 1 -->
+                        <div class="main-page-work-grid">
+                            <div class="col-1of3-work-main">
+                                
+
+                                <div class="col-1of3-work-figure">
+                                    <div class="main-work-listing-figure">
+                                        <figure>
+                                            <a href="<?php echo page('work/bccampus') ?>"><img class="work-listing-img" src="content/home/bcc-website-screenshot-cropped.jpg" alt="BCcampus website screenshot"></a>
+                                        </figure>
+                                    </div>
                                 </div>
-                            </div>
-                            
-                            <div class="col-2of3-work-description">
-                                <div class="work-description">
-                                    <h3><a href="<?php echo page('work/bccampus') ?>">BCcampus.ca Usability Testing</a></h3>
-                                    <em>UX research &bull; usability testing</em>
-                                    <p>
-                                        I created and facilitated a usability test for the BCcampus.ca website to find out whether the audience could perform typical tasks on the website easily, and to discover usability problems.
-                                    </p>
-                                </div> <!-- ./work-description -->
-                            </div> <!-- ./col-2of3-work-description -->
-                        </div> <!-- ./col-1of3-work-main -->
-                    </div>
+                                
+                                <div class="col-2of3-work-description">
+                                    <div class="work-description">
+                                        <h3><a href="<?php echo page('work/bccampus') ?>">BCcampus.ca Usability Testing</a></h3>
+                                        <em>UX research &bull; usability testing</em>
+                                        <p>
+                                            I created and facilitated a usability test for the BCcampus.ca website to find out whether the audience could perform typical tasks on the website easily, and to discover usability problems.
+                                        </p>
+                                    </div> <!-- ./work-description -->
+                                </div> <!-- ./col-2of3-work-description -->
+                            </div> <!-- ./col-1of3-work-main -->
+                        </div>
 
-                    <div class="main-page-work-grid">
-                        <div class="col-1of3-work-main">
-                            
-                            <div class="col-1of3-work-figure">
-                                <div class="main-work-listing-figure">
-                                    <figure class="notif-image">
-                                        <a href="<?php echo page('work/notif') ?>"><img class="work-listing-img" src="content/home/notif-thumb.png" alt="Notif use case screenshot"></a>
-                                    </figure>
-                                </div> <!-- ./main-work-listing-figure -->
-                            </div> <!-- ./col-1of3-work-figure -->
+                        <!-- item 2 -->
+                        <div class="main-page-work-grid">
+                            <div class="col-1of3-work-main">
+                                
+                                <div class="col-1of3-work-figure">
+                                    <div class="main-work-listing-figure">
+                                        <figure>
+                                            <a href="<?php echo page('work/notif') ?>"><img class="work-listing-img" src="content/home/notif-thumb.png" alt="Notif use case screenshot"></a>
+                                        </figure>
+                                    </div> <!-- ./main-work-listing-figure -->
+                                </div> <!-- ./col-1of3-work-figure -->
 
-                            <div class="col-2of3-work-description">
-                                <div class="work-description">
-                                    <h3><a href="<?php echo page('work/notif') ?>">Notif Concept Mapping Tool</a></h3>
-                                    <em>UX research &amp; development &bull; usability testing &bull; programming</em>
-                                    <p>
-                                        Prototyping a concept mapping tool targeted towards the academic teaching and learning domain. The design and functionality of our tool was heavily use-case and scenario-driven.
-                                    </p>
+                                <div class="col-2of3-work-description">
+                                    <div class="work-description">
+                                        <h3><a href="<?php echo page('work/notif') ?>">Notif Concept Mapping Tool</a></h3>
+                                        <em>UX research &amp; development &bull; usability testing &bull; programming</em>
+                                        <p>
+                                            Prototyping a concept mapping tool targeted towards the academic teaching and learning domain. The tool's design and functionality was heavily use-case and scenario-driven.
+                                        </p>
 
-                                </div> <!-- ./work-description -->
-                            </div> <!-- ./col-2of3-work-description -->
+                                    </div> <!-- ./work-description -->
+                                </div> <!-- ./col-2of3-work-description -->
 
-                        </div> <!-- ./col-1of3-work-main -->
-                    </div> <!-- ./main-page-work-grid -->
+                            </div> <!-- ./col-1of3-work-main -->
+                        </div> <!-- ./main-page-work-grid -->
+
+                        <!-- item 3 -->
+                        <div class="main-page-work-grid">
+                            <div class="col-1of3-work-main">
+                                
+                                <div class="col-1of3-work-figure">
+                                    <div class="main-work-listing-figure">
+                                        <figure>
+                                            <a href="<?php echo page('work/sprawl-visualization') ?>"><img class="work-listing-img" src="content/home/sprawl-visualization-cropped.jpg" alt="Urban Sprawl Visualization screenshot"></a>
+                                        </figure>
+                                    </div> <!-- ./main-work-listing-figure -->
+                                </div> <!-- ./col-1of3-work-figure -->
+
+                                <div class="col-2of3-work-description">
+                                    <div class="work-description">
+                                        <h3><a href="<?php echo page('work/sprawl-visualization') ?>">Urban Sprawl Visualization</a></h3>
+                                        <em>data visualization &bull; information design &bull; web programming</em>
+                                        <p>
+                                            Visualization about urban sprawl statistics in the United States, built using JavaScript.
+                                        </p>
+
+                                    </div> <!-- ./work-description -->
+                                </div> <!-- ./col-2of3-work-description -->
+
+                            </div> <!-- ./col-1of3-work-main -->
+                        </div> <!-- ./main-page-work-grid -->
+                    </div> <!-- ./main-page-work-row -->
+
+                    <div class="main-page-work-row">
+                        <!-- item 4 -->
+                        <div class="main-page-work-grid">
+                            <div class="col-1of3-work-main">
+                                
+                                <div class="col-1of3-work-figure">
+                                    <div class="main-work-listing-figure">
+                                        <figure class="notif-image">
+                                            <a href="<?php echo page('work/notif') ?>"><img class="work-listing-img" src="content/home/notif-thumb.png" alt="Notif use case screenshot"></a>
+                                        </figure>
+                                    </div> <!-- ./main-work-listing-figure -->
+                                </div> <!-- ./col-1of3-work-figure -->
+
+                                <div class="col-2of3-work-description">
+                                    <div class="work-description">
+                                        <h3><a href="<?php echo page('work/notif') ?>">Notif Concept Mapping Tool</a></h3>
+                                        <em>UX research &amp; development &bull; usability testing &bull; programming</em>
+                                        <p>
+                                            Prototyping a concept mapping tool targeted towards the academic teaching and learning domain. The design and functionality of our tool was heavily use-case and scenario-driven.
+                                        </p>
+
+                                    </div> <!-- ./work-description -->
+                                </div> <!-- ./col-2of3-work-description -->
+
+                            </div> <!-- ./col-1of3-work-main -->
+                        </div> <!-- ./main-page-work-grid -->
+
+                        <!-- item 5 -->
+                        <div class="main-page-work-grid">
+                            <div class="col-1of3-work-main">
+                                
+                                <div class="col-1of3-work-figure">
+                                    <div class="main-work-listing-figure">
+                                        <figure class="notif-image">
+                                            <a href="<?php echo page('work/notif') ?>"><img class="work-listing-img" src="content/home/notif-thumb.png" alt="Notif use case screenshot"></a>
+                                        </figure>
+                                    </div> <!-- ./main-work-listing-figure -->
+                                </div> <!-- ./col-1of3-work-figure -->
+
+                                <div class="col-2of3-work-description">
+                                    <div class="work-description">
+                                        <h3><a href="<?php echo page('work/notif') ?>">Notif Concept Mapping Tool</a></h3>
+                                        <em>UX research &amp; development &bull; usability testing &bull; programming</em>
+                                        <p>
+                                            Prototyping a concept mapping tool targeted towards the academic teaching and learning domain. The design and functionality of our tool was heavily use-case and scenario-driven.
+                                        </p>
+
+                                    </div> <!-- ./work-description -->
+                                </div> <!-- ./col-2of3-work-description -->
+
+                            </div> <!-- ./col-1of3-work-main -->
+                        </div> <!-- ./main-page-work-grid -->
+
+                        <!-- item 6 -->
+                        <div class="main-page-work-grid">
+                            <div class="col-1of3-work-main">
+                                
+                                <div class="col-1of3-work-figure">
+                                    <div class="main-work-listing-figure">
+                                        <figure class="notif-image">
+                                            <a href="<?php echo page('work/notif') ?>"><img class="work-listing-img" src="content/home/notif-thumb.png" alt="Notif use case screenshot"></a>
+                                        </figure>
+                                    </div> <!-- ./main-work-listing-figure -->
+                                </div> <!-- ./col-1of3-work-figure -->
+
+                                <div class="col-2of3-work-description">
+                                    <div class="work-description">
+                                        <h3><a href="<?php echo page('work/notif') ?>">Notif Concept Mapping Tool</a></h3>
+                                        <em>UX research &amp; development &bull; usability testing &bull; programming</em>
+                                        <p>
+                                            Prototyping a concept mapping tool targeted towards the academic teaching and learning domain. The design and functionality of our tool was heavily use-case and scenario-driven.
+                                        </p>
+
+                                    </div> <!-- ./work-description -->
+                                </div> <!-- ./col-2of3-work-description -->
+
+                            </div> <!-- ./col-1of3-work-main -->
+                        </div> <!-- ./main-page-work-grid -->
+                    </div> <!-- ./main-page-work-row -->
+
 
                 </div> <!-- ./grid (3-column) -->
