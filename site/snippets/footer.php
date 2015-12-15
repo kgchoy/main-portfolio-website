2c111291cf925a8a64ef967694c8b11db824486a
Kevin Choy <kevinc.2052@gmail.com>
2015-12-15 03:12:33 -0800

    updated footer email

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -6,7 +6,7 @@
                 <div id="footer-bar-left">
                     <ul class="menu-items-list" id="left-footer-links">
                         <li>
-                            <a class="button-nav button-footer-nav" href="mailto:kgchoy@sfu.ca"><img class="footer-icon" src="<?php echo url('assets/images/icon-email.png') ?>" alt="Email icon" width="32" height="18" aria-hidden="true">Email</a>
+                            <a class="button-nav button-footer-nav" href="mailto:kevinc.2052@gmail.com"><img class="footer-icon" src="<?php echo url('assets/images/icon-email.png') ?>" alt="Email icon" width="32" height="18" aria-hidden="true">Email</a>
                         </li>
                         <li>
                             <a class="button-nav button-footer-nav" href="http://ca.linkedin.com/pub/kevin-choy/93/a52/784/"><img class="footer-icon" src="<?php echo url('assets/images/icon-linkedin.png') ?>" alt="Email icon" width="27" height="18" aria-hidden="true">LinkedIn</a>
