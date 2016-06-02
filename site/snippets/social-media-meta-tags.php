51e68bfe3b155bbc87152a61e89e472a4422b1d1
Kevin Choy <kevinc.2052@gmail.com>
2016-06-02 01:36:20 -0700

    updated home page's open graph tags, and temporarily removed twitter card tags

--- site/snippets/social-media-meta-tags.php
+++ site/snippets/social-media-meta-tags.php
@@ -1,31 +1,3 @@
-<!-- Twitter Cards -->
-<?php if($page->twitterCard() != ""): ?>
-  <meta name="twitter:card" content="<?php echo $page->twitterCard() ?>">
-<?php endif ?>
-
-<?php if($page->twitterSite() != ""): ?>
-  <meta name="twitter:site" content="<?php echo $page->twitterSite() ?>">
-<?php endif ?>
-
-<?php if($page->twitterCreator() != ""): ?>
-  <meta name="twitter:creator" content="<?php echo $page->twitterCreator() ?>">
-<?php endif ?>
-
-<?php if($page->twitterTitle() != ""): ?>
-  <meta name="twitter:title" content="<?php echo $page->twitterTitle() ?>">
-<?php endif ?>
-
-<?php if($page->twitterDescription() != ""): ?>
-  <meta name="twitter:description" content="<?php echo $page->twitterDescription() ?>">
-<?php endif ?>
-
-<?php if($page->twitterImage() != ""): ?>
-  <meta name="twitter:image" content="<?php echo $page->twitterImage() ?>">
-<?php endif ?>
-
-
-
-
 <!-- Open Graph -->
 <?php if($page->OpenGraphTitle() != ""): ?>
   <meta property="og:title" content="<?php echo $page->OpenGraphTitle() ?>">
